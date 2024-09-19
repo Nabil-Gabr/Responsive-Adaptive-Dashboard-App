ef67d1fe65b96213b9634b45ac223c9b49b6d603
Nabil-Gabr <nabiljbr490@gmailcom>
2024-09-19 23:19:45 +0300

    add app styles

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,16 @@
+import 'package:dash_board_app/views/widgets/adaptive_layout.dart';
+import 'package:dash_board_app/views/widgets/dashboard_desktop_layout.dart';
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      );
+  }
+}
