d9a4830390183d1da0c9a1c33b7a0c685b6febe6
Nabil-Gabr <nabiljbr490@gmailcom>
2024-09-20 11:40:57 +0300

    add app asset

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// asset/images/avatar_1.svg
+  static const String imagesAvatar1 = "asset/images/avatar_1.svg";
+
+  /// Assets for imagesAvatar2
+  /// asset/images/avatar_2.svg
+  static const String imagesAvatar2 = "asset/images/avatar_2.svg";
+
+  /// Assets for imagesAvatar3
+  /// asset/images/avatar_3.svg
+  static const String imagesAvatar3 = "asset/images/avatar_3.svg";
+
+  /// Assets for imagesBalance
+  /// asset/images/balance.svg
+  static const String imagesBalance = "asset/images/balance.svg";
+
+  /// Assets for imagesCardBackground
+  /// asset/images/card_background.png
+  static const String imagesCardBackground = "asset/images/card_background.png";
+
+  /// Assets for imagesDashboard
+  /// asset/images/dashboard.svg
+  static const String imagesDashboard = "asset/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// asset/images/expenses.svg
+  static const String imagesExpenses = "asset/images/expenses.svg";
+
+  /// Assets for imagesGallery
+  /// asset/images/gallery.svg
+  static const String imagesGallery = "asset/images/gallery.svg";
+
+  /// Assets for imagesIncome
+  /// asset/images/income.svg
+  static const String imagesIncome = "asset/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// asset/images/logout.svg
+  static const String imagesLogout = "asset/images/logout.svg";
+
+  /// Assets for imagesMyInvestments
+  /// asset/images/my_investments.svg
+  static const String imagesMyInvestments = "asset/images/my_investments.svg";
+
+  /// Assets for imagesMyTransctions
+  /// asset/images/my_transctions.svg
+  static const String imagesMyTransctions = "asset/images/my_transctions.svg";
+
+  /// Assets for imagesSettings
+  /// asset/images/settings.svg
+  static const String imagesSettings = "asset/images/settings.svg";
+
+  /// Assets for imagesStatistics
+  /// asset/images/statistics.svg
+  static const String imagesStatistics = "asset/images/statistics.svg";
+
+  /// Assets for imagesWalletAccount
+  /// asset/images/wallet_account.svg
+  static const String imagesWalletAccount = "asset/images/wallet_account.svg";
+}
+
